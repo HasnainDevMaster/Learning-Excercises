f261f4d7961eed6f2c291cfb9976c3db3c6ec9c6
Syed Hasnain Ali Shah <syedhasnainali927@gmail.com>
2024-08-07 09:26:20 +0500

    Update app.ts

--- Ex 26 Alien Colors #2/app.js
+++ Ex 26 Alien Colors #2/app.js
@@ -1,14 +1,22 @@
 "use strict";
-let alien_color = "red";
-if (alien_color === "red") {
-    console.log("You just earned 5 points for shooting the red alien.");
-}
-else {
-    console.log("you just earned 10 points.");
-}
+// Version 1: Alien color is green, runs the if block
+let alien_color = "green"; // Declare a variable alien_color and assign it a value of 'green'
 if (alien_color === "green") {
-    console.log("You just earned 5 points for shooting the red alien.");
+    // Check if the alien's color is green,The player earns 5 points for shooting a green alien
+    console.log("Congratulations! You just earned 5 points for shooting down a green alien.");
 }
 else {
-    console.log("you just earned 10 points.");
+    // This block will not be executed in this scenario
+    console.log("Congratulations! You just earned 10 points for shooting the alien!");
+}
+// Version 2: Alien color is yellow, runs the else block
+let alien_Color_fail = "yellow"; // Assigning an alien's color to 'yellow'
+if (alien_Color_fail === "green") {
+    // Check if the alien's color is green
+    // This block will not be executed in this scenario
+    console.log("Congratulations! You just earned 5 points for shooting down a green alien.");
+}
+else {
+    // The player earns 10 points for shooting a non-green alien
+    console.log("Congratulations! You just earned 10 points for shooting the alien!");
 }
